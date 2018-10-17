7adf88cc767a190a07108b724af3dd7d2abdf0a6
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-10-17 10:05:38 +0000

    P0.Halló Heimur

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,12 @@
 // Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(300,300);
+    background(600,100,359);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(30);
+    text("Halló heimur",64,40);
+    rect(150,150,100,100)
+    ellipse(100,200,50,50);
+}
