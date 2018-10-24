e1974569873edc53bdc47d81b8eb4198834fd568
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-10-24 08:50:05 +0000

    bæti við p2-andlit

---
+++ P2-Andlit/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(900,900);
+    background(255,255,255);
+    fill(255,0,0);
+    rect(0,0,400,500);//hyrningar
+    fill(255);
+    rect(0,500,400,400);
+    fill(255);
+    rect(400,0,500,500);
+    fill(0);
+  }
