b3b2cd933b5ebadb361a8e38e6ab5f5327da572e
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-10-26 13:21:08 +0000

    update

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -1,12 +1,37 @@
-// Hér kemur kóðinn þinn:
+var eyeSize = 20;
+var faceWidth = 100;
+var faceHeight = 150;
+var x = 150;
+var y = 150;
+
+
 function setup(){
-    createCanvas(900,900);
-    background(255,255,255);
-    fill(255,0,0);
-    rect(0,0,400,500);//hyrningar
-    fill(255);
-    rect(0,500,400,400);
-    fill(255);
-    rect(400,0,500,500);
-    fill(0);
-  }
+  createCanvas (300,300);
+}
+
+function draw(){
+  background(255,0,0);
+  ellipse(x, y, faceWidth, faceHeight);
+
+  var vinstra_auga = width/2 - faceWidth*0.25;
+  var hægra_auga = width/2 + faceWidth*0.25;
+  ellipse(vinstra_auga, height/2, eyeSize, eyeSize);
+  ellipse(hægra_auga, height/2, eyeSize, eyeSize);
+
+  arc(160, 169, 20, 16, 2, PI + QUARTER_PI);
+
+  arc(153, 194, 20, 8, 180, PI + QUARTER_PI);
+
+
+
+
+  //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
+
+}
+
+function mousePressed(){
+  //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+  faceWidth  = random(75,  150);
+  faceHeight = random(100, 200);
+  eyeSize    = random(10,  30);
+}
