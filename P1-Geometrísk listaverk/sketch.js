a3afb4bb01494da15b156f233f56641bdf78bba9
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-10-26 14:02:12 +0000

    Update á geómetríska listaverkinu-klárað

--- P1-Geometrísk listaverk/sketch.js
+++ P1-Geometrísk listaverk/sketch.js
@@ -2,11 +2,19 @@
 function setup(){
     createCanvas(900,900);
     background(255,255,255);
+    strokeWeight(15); // Beastly
     fill(255,0,0);
     rect(0,0,400,500);//hyrningar
     fill(255);
     rect(0,500,400,400);
     fill(255);
     rect(400,0,500,500);
+    fill(0,0,255);
+    rect(825,500,75,400);
+    fill(255);
+    rect(825,650,75,250);
+    fill(255,255,0);
+    rect(400,850,200,50);
     fill(0);
+    rect(600,850,225,50);
   }
--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -3,6 +3,10 @@ var faceWidth = 100;
 var faceHeight = 150;
 var x = 150;
 var y = 150;
+var augah = 150 + faceWidth*0.25;
+var augav = 150 - faceWidth*0.25;
+var munnurl = 255;
+var litur = 0;
 
 
 function setup(){
@@ -10,28 +14,29 @@ function setup(){
 }
 
 function draw(){
-  background(255,0,0);
+  background(180);
+  fill(0);
+  rect(150-faceWidth/2, 150, faceWidth, faceHeight/2);
+  fill(255);
   ellipse(x, y, faceWidth, faceHeight);
-
-  var vinstra_auga = width/2 - faceWidth*0.25;
-  var hægra_auga = width/2 + faceWidth*0.25;
-  ellipse(vinstra_auga, height/2, eyeSize, eyeSize);
-  ellipse(hægra_auga, height/2, eyeSize, eyeSize);
-
-  arc(160, 169, 20, 16, 2, PI + QUARTER_PI);
-
-  arc(153, 194, 20, 8, 180, PI + QUARTER_PI);
-
-
-
-
+  fill(0);
+  arc(x, y, faceWidth, faceHeight, PI, 0);
+  ellipse(augav, height/2, eyeSize, eyeSize);
+  ellipse(augah, height/2, eyeSize, eyeSize);
+  fill(munnurl);
+  fill (litur);
+  arc(x, y+30, faceWidth/2, 30, 0, PI);
+  fill(255);
+  arc(x, y+10, faceWidth/5, faceHeight/4, 0, PI);
   //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
-
+  
 }
-
+tóti er fá
 function mousePressed(){
   //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
   faceWidth  = random(75,  150);
   faceHeight = random(100, 200);
   eyeSize    = random(10,  30);
+  munnurl 	 = random(0, 255);
+  litur = int(random(-1,1));
 }
