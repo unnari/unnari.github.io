445ab046fb85e767fd7748c5869c0794fc68218e
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-10-29 12:35:23 +0000

    bætt við p1-b

---
+++ P1b-Geometrísk listaverk/sketch.js
@@ -0,0 +1,146 @@
+    // Hér kemur kóðinn þinn:
+    function setup(){
+    createCanvas(800,800);
+    background(255);
+    strokeWeight(0); // Beastly
+    //fyrsta lína
+    fill(0,0,255);
+    rect(0,0,100,100);//hyrningar
+    fill(255,0,0);
+    rect(100,0,100,100);
+    fill(52,107,46); //tri
+    rect(200,0,100,100);//hyrningar
+    fill(52,107,46);
+    rect(300,0,100,100);
+    fill(255,165,0); //tri
+    rect(400,0,100,100);//hyrningar
+    fill(255,165,0);
+    rect(500,0,100,100);
+    fill(255,0,0);
+    rect(600,0,100,100);//hyrningar
+    fill(0,0,255);
+    rect(700,0,100,100);
+    //önnur lína lala
+    fill(0);
+    rect(0,100,100,100);//hyrningar
+    fill(255,0,0);
+    rect(100,100,100,100);
+    fill(0);
+    rect(200,100,100,100);//hyrningar
+    fill(0);
+    rect(300,100,100,100);
+    fill(0);
+    rect(400,100,100,100);//hyrningar
+    fill(255,165,0); //tri
+    rect(500,100,100,100);
+    fill(255,0,0);
+    rect(600,100,100,100);//hyrningar
+    fill(121,96,76);
+    rect(700,100,100,100);
+    //þriðja lína
+    fill(0);
+    rect(0,200,100,100);//hyrningar
+    fill(255,0,0);
+    rect(100,200,100,100);
+    fill(0); //tri
+    rect(200,200,100,100);//hyrningar
+    fill(0);
+    rect(300,200,100,100);
+    fill(0);
+    rect(400,200,100,100);//hyrningar
+    fill(255,165,0);
+    rect(500,200,100,100);
+    fill(255,0,0);
+    rect(600,200,100,100);//hyrningar
+    fill(121,96,76);
+    rect(700,200,100,100);
+    //fjórða lína
+    fill(0);
+    rect(0,300,100,100);//hyrningar
+    fill(255,165,0);
+    rect(100,300,100,100);
+    fill(255);
+    rect(200,300,100,100);//hyrningar
+    fill(255);
+    rect(300,300,100,100);
+    fill(255,0,0);
+    rect(400,300,100,100);//hyrningar
+    fill(255,0,0);
+    rect(500,300,100,100);
+    fill(121,96,76);
+    rect(600,300,100,100);//hyrningar
+    fill(121,96,76);
+    rect(700,300,100,100);
+    //seinni helmingur
+    //fyrsta lína
+    fill(0);
+    rect(0,400,100,100);
+    fill(255,165,0);
+    rect(100,400,100,100);
+    fill(255);
+    rect(200,400,100,100)
+    fill(255);
+    rect(300,400,100,100);
+    fill(255,0,0);
+    rect(400,400,100,100);
+    fill(255,0,0);
+    rect(500,400,100,100);
+    fill(121,96,76);//brúnn
+    rect(600,400,100,100);
+    fill(255);
+    rect(700,400,100,100);
+    //önnur lína
+    fill(255);
+    rect(0,500,100,100);
+    fill(255,0,0);
+    rect(100,500,100,100);
+    fill(255);
+    rect(200,500,100,100);
+    fill(255);
+    rect(300,500,100,100);
+    fill(255);
+    rect(400,500,100,100);
+    fill(255,0,0);
+    rect(500,500,100,100);
+    fill(255,0,0);
+    rect(600,500,100,100);
+    fill(255);
+    rect(700,500,100,100);
+    //þriðja lína
+    fill(255);
+    rect(0,600,100,100);
+    fill(255,0,0);
+    rect(100,600,100,100);
+    fill(255);
+    rect(200,600,100,100);
+    fill(255);
+    rect(300,600,100,100);
+    fill(255);
+    rect(400,600,100,100);
+    fill(255,255,0);
+    rect(500,600,100,100);//hyrningur
+    fill(255,0,0);
+    rect(600,600,100,100);
+    fill(255);
+    rect(700,600,100,100);
+    //fjórða lína
+    fill(0,0,255);
+    rect(0,700,100,100);
+    fill(255,0,0);
+    rect(100,700,100,100);
+    fill(255,255,0);
+    rect(200,700,100,100);
+    fill(255,255,0);
+    rect(300,700,100,100);//hyrningur
+    fill(255);
+    rect(400,700,100,100);//hyrningur
+    fill(255,255,0);
+    rect(500,700,100,100);
+    fill(255,0,0);
+    rect(600,700,100,100);
+    fill(0,0,255);
+    rect(700,700,100,100);
+
+
+
+  }
