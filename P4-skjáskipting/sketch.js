5eaded85d2baa9a21dd676b7714aef0891928378
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-11-05 12:32:34 +0000

    bætti við p4 - þarf að klára

---
+++ P4-skjáskipting/sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+	createCanvas(700, 500);
+	noStroke();
+}
+
+function draw() {
+	background(255, 0, 0);
+	fill(255, 255, 0);
+	if(mouseX < width/2 && mouseY < height / 2){
+		rect(0,0,width/2,height/2);
+    <html>
+      <body>
+       <p>Uppi til vinstri </p>
+  	}
+	if(mouseX > width/2 && mouseY < height / 2){
+		rect(width/2,0,width/2,height/2);
+	}
+	if(mouseX < width/2 && mouseY > height / 2){
+		rect(0,height/2,width/2,height/2);
+	}
+	if(mouseX > width/2 && mouseY > height / 2){
+		rect(width/2,height/2,width/2,height/2);
+	}
+}
