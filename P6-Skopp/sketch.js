bd09310ccb3f072ace99ecbdf584384fa4e3e03e
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-11-12 11:51:14 +0000

    bætt við p6

---
+++ P6-Skopp/sketch.js
@@ -0,0 +1,43 @@
+var x = 100;
+var y = 100;
+var hradiX = 5;
+var hradiY = 11;
+var bolti = 20;
+var stig = 0;
+var spadiBreidd = 150;
+var spadiThykkt = 25;
+var spadiY;
+
+function setup() {
+	createCanvas(600,500);
+	spadiY = height - 45;
+	rectMode(CENTER);
+	textFont("Courier New",16);
+	textStyle(BOLD);
+}
+
+function draw() {
+  background(333,33,10);
+	// Reikna ný hnit út frá hraða boltans:
+  x = x + hradiX;
+  y = y + hradiY;
+	// Athuga hvort boltinn snertir veggi:
+  if ((x > width-bolti/2) || (x < bolti/2))  {
+    hradiX = hradiX * -1;
+  }
+  if ((y > height-bolti/2) || (y < bolti)) {
+    hradiY = hradiY * -1;
+  }
+	// Athuga hvort bolti snertir spaða:
+	// abs() táknar algildi/tölugildi
+	if(abs(spadiY - y) < bolti/2 + spadiThykkt/2 && abs(mouseX - x) < bolti/2 + spadiBreidd/2){
+    hradiY = hradiY * -1;
+		stig = stig + 1;
+	}
+	// Teikna boltann, spaðann og stigin
+  fill(255,255,0);
+  rect(x, y, bolti, bolti);
+	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
+	fill(0);
+	text("Stig: " + stig,20,20);
+}
