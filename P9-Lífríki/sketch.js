6351eae1b6113bc79006d1ccb2894276fbd8ae28
Unnar Ingvarsson <unnaringvars@gmail.com>
2018-11-26 11:54:58 +0000

    p9 breytt

--- P9-Lífríki/sketch.js
+++ P9-Lífríki/sketch.js
@@ -1,101 +1,84 @@
-var zoogs = [];
+var unnsi, unnsi2, unnsi3
 
 function setup() {
-  createCanvas(600, 600);
-  // Búum til hundrað zoog hluti.
-  for (var i = 0; i < 100; i = i+1){
-    zoogs[i] = new Zoog(random(200,400),random(100,400),random(50,200));
-  }
+  createCanvas(600,600);
+  Uaquok1 = new Uaquok(150,150,150,100,150);
+  Uaquok2 = new Uaquok(100,100,30,120,100);
+  Uaquok3 = new Uaquok(350,300,250,150,50);
 }
 
 function draw() {
-	background(200,150,0);
-  fill(255,200,0);
+  background(234,124,21);
+  fill(532,123,43);
 	ellipse(300,300,500,500);
-  // Teiknum og hreyfum alla Zoog kallana okkar
-  for (var i = 0; i < zoogs.length; i = i+1){
-  	zoogs[i].move();
-  	zoogs[i].show();
-  }
+  Uaquok1.move();
+  Uaquok1.show();
+  Uaquok2.move();
+  Uaquok2.show();
+  Uaquok3.move();
+  Uaquok3.show();
 }
 
-function mousePressed(){
-  for (var i = zoogs.length-1; i >= 0; i = i-1){
-    if(fjarl(zoogs[i].x,zoogs[i].y,mouseX,mouseY) < 50){
-		    zoogs.splice(i,1);
-	  }
-  }
-}
-
-// Klasinn Zoog
-// (x,y): er miðpunkturinn sem hann er teiknaður útfrá.
-// bukur: er lengdin á búknum
-// dir: er stefnan sem hann ferðast í
-// speed: er hraðinn sem hann ferðast á
-// eyecolor: er liturinn á augunum hans
-class Zoog{
-  constructor(x,y,bukur) {
+class Unnsi{
+  constructor(x,y,bukur,haus,litur){
     this.x = x;
     this.y = y;
     this.bukur = bukur;
-		this.dir = random(0,2*PI);
-		this.speed = random(1,3);
-		this.eyeColor = color(100,100,100);
+    this.haus = haus;
+    this.dir = random(0,2*PI);
+    this.speed = random(0,5);
+    this.bukurlitur = color(150,300,56);
   }
 
-  // Teiknar geimveruna zoog
-  show(){
-    rectMode(CENTER);
-    // Teiknum búkinn
-      rectMode(CENTER);
-      // Teiknum búkinn
-      fill(44,44,44);
-      rect(this.x,this.y,70,this.bukur)
-      // Teiknum hausinn
-      fill(255);
-      ellipse(this.x,this.y-this.bukur/2, 80,80);
-      // Teiknum augun
-      fill(this.eyecolor);
-      ellipse(this.x - 25, this.y - this.bukur/2, 20,25);
-      ellipse (this.x + 25,this.y - this.bukur/2, 20,25);
-      //Munnur
-      fill(236,36,94);
-      ellipse(200,170, 25, 20);
-      // Teiknum fæturna
-      line (180, 250, 180, 300);
-      line (220, 250, 220, 300);
-      line (200, 250, 200, 270);
-    }
+show() {
+  rectMode(CENTER);
+  strokeWeight(2);
+  stroke(0);
+	// búkur
+	fill(this.bukurlitur);
+	ellipse(this.x,this.y,-250,this.bukur)
+  // haus
+	fill(70,150,0)
+	rect(this.x,this.y - this.bukur/2, this.haus,this.haus)
+	//fætur
+	fill(70,150,0)
+	line (this.x - 20, this.y + this.bukur/2, this.x - 50, this.y + this.bukur/2 + 75);
+  line (this.x + 20, this.y + this.bukur/2, this.x + 50, this.y + this.bukur/2 + 75);
+	//hendur
+	fill(0)
+	line (this.x - 20, this.y , this.x - 20, this.y + 40);
+  line (this.x + 20, this.y , this.x + 20, this.y + 40);
+	// augu
+	fill(0)
+	ellipse (this.x - 50,this.y - this.bukur/2, 20,40);
+  ellipse (this.x + 50,this.y - this.bukur/2, 20,40);
 
-  // Hreyfir zoog um skjáinn.
-  // Hann minnkar sífelt hraðann, en velur svo slembistenfu
-  // og fyglir henni á nyjum slbenum hraða.
-	move(){
+ }
+
+ move(){
 		this.x = this.x + this.speed*cos(this.dir);
 		this.y = this.y + this.speed*sin(this.dir);
-		this.speed = this.speed/1.05;
-		if(this.speed < 0.1){
+		this.speed = this.speed/1.07;
+		if(this.speed < 0.3){
 			this.dir = random(0,2*PI);
-			this.speed = random(1,5);
-		}
-      if (fjarl(this.x,this.y,width/2,height/2) > 250){
-      	this.blink();
-        this.dir = atan((this.y - height/2)/(this.x-width/2));
-        if(this.x - width/2 > 0){
-        	this.dir = this.dir + PI;
-        }
-      }
-	}
-
-  // Velur slembinn lit fyrir augun á zoog.
-	blink(){
-		this.eyeColor = color(random(255), random(255), random(255));
-	}
+			this.speed = random(1,7);
+    }
+    if (fjarlægð(this.x,this.y,width/2,height/2) > 250){
+      this.litur();
+      this.dir = atan((this.y - height/2)/(this.x-width/2));
+      if(this.x - width/2 > 0){
+        this.dir = this.dir + PI;
+    }
+  }
 }
 
-// Skilar fjarlægðinni á milli punktanna
-// (x1,y1) og (x2,y2).
-function fjarl(x1,y1,x2,y2){
+litur(){
+		this.bukurlitur = color(random(255), random(255), random(255));
+
+  }
+}
+
+function fjarlægð(x1,y1,x2,y2){
   var dist = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
   return dist;
 }
